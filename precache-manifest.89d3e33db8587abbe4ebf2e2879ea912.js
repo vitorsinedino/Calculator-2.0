8570ba07a7941e27986106c3a7080bc63de08757
vitor.sinedino <vitorsinedino@gmail.com>
2020-04-14 11:23:04 -0300

    Updates

---
+++ precache-manifest.89d3e33db8587abbe4ebf2e2879ea912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f6e3a85713f6e6b250d775b5971ebc",
+    "url": "/Calculator-2.0/index.html"
+  },
+  {
+    "revision": "9fe07c349921d04a938e",
+    "url": "/Calculator-2.0/static/css/main.ff813cbb.chunk.css"
+  },
+  {
+    "revision": "9936a7b1f30e7d941ea1",
+    "url": "/Calculator-2.0/static/js/2.db848ff9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Calculator-2.0/static/js/2.db848ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe07c349921d04a938e",
+    "url": "/Calculator-2.0/static/js/main.68de4861.chunk.js"
+  },
+  {
+    "revision": "4889d618968d70ebdbb3",
+    "url": "/Calculator-2.0/static/js/runtime-main.e8bb65fb.js"
+  }
+]);
